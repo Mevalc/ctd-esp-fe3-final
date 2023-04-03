7defae88e81180a0a87da99d27d23d90070c0f74
Mel <mevalc@gmail.com>
2023-04-03 16:38:37 -0500

    primer paso

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import {Route, Routes}  from 'react-router-dom';
+import Home from "./Routes/Home";
+import Dentist from "./Routes/Dentist";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs"
+import Form from "./Components/Form";
 
 
 function App() {
@@ -8,8 +14,23 @@ function App() {
       <div className="App">
           <Navbar/>
           <Footer/>
+
+          <Routes>
+            <Route path='/' element ={<Home/>}/>
+            <Route path='/dentista' element ={<Dentist/>}/>
+            <Route path='/contacto' element ={<Contact/>}/>
+            <Route path='/favoritos' element ={<Favs/>}/>
+
+          </Routes>
+          <Form/>
       </div>
   );
 }
 
 export default App;
+
+
+
+
+// El routes es para las rutas de la navegacion cuando aparace /contacto/alumno etc
+//me falta poner la ruta con id del dentista <Route path="/dentist/:id" component={DentistDetail} />
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
 
+  const [user, setUser] = useState({
+    nombre: "",
+    email: "",
+  });
+
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+  //para que valide que es formato correo electronico
+  const validarEmail = /\S+@\S+\.\S+/;
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (user.nombre.length >=5 && validarEmail.test(user.email)){
+      setShow(true)
+      setError(false)
+    } else {
+      setShow(false)
+      setError(true)
+    }
+  };
+
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
+
+      <label>Nombre: </label>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, nombre: e.target.value })}
+        />
+        <label>Email: </label>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+
+        <button className="submit">Enviar</button>
+
       </form>
+
+      {error ? 'Por favor verifique su información nuevamente' : null}
+      {show ?  
+        <h3>
+          Gracias {user.nombre} te contactaremos cuando antes vía mail.
+        </h3> : null}
+     
     </div>
   );
 };
 
 export default Form;
+
+
